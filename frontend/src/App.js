d8427575f21d07d90f944f3d994385f151746c3b
Mounika Karasu <mounikakarasu@gmail.com>
2026-03-17 19:54:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
   const fetchDecision = async () => {
     try {
-      const API_URL = process.env.REACT_APP_API_URL || "http://13.62.104.103:8000";
+      const API_URL = process.env.REACT_APP_API_URL || "http://16.171.42.244:8000";
 const response = await fetch(`${API_URL}/predict`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
